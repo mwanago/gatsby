0d2bcbfe21f3a9cdf35f505a07b4f354d789f6d7
marcin <wanago.marcin@gmail.com>
2022-11-27 23:28:20 +0100

    feat: link from the about page to the index page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,12 @@
 import React, { FunctionComponent } from 'react';
 import type { HeadFC, PageProps } from 'gatsby';
+import { Link } from 'gatsby';
 
 const AboutPage: FunctionComponent<PageProps> = () => {
   return (
     <main>
       <h1>About me page</h1>
+      <Link to="/">Go to the main page</Link>
     </main>
   );
 };
